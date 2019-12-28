5774f81ef383dece49e65d21e43802bc69645029
tehnarenok <tehnarenok@gmail.com>
2019-12-28 20:38:18 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react'
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = lazy(() => import('./App'))
+
+ReactDOM.render(
+    <Router>
+        <Suspense fallback={<div>Загрузка...</div>}>
+            <Switch>
+                <Route exact path='/' component={App}/>
+                <Route exact path='/home/' component={App}/>
+            </Switch>
+        </Suspense>
+    </Router>
+    , document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
